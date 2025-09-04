a16c5bef88196b7de8289a31c90ba5ea41a4bb3a
angelvm20 <angelchino757@gmail.com>
2025-09-04 15:45:18 -0500

    configuracion base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pagina_webPipo/', // nombre del repositorio para despliegue (github)
 })
